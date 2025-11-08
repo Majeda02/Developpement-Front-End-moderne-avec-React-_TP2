f1cd1a761adddc0efd7088251f9dc373b7a281ee
Majeda02 <benlaghfirimajeda@gmail.com>
2025-11-08 22:12:50 +0100

    commit 2

--- src/FormulaireInscription.js
+++ src/FormulaireInscription.js
@@ -2,20 +2,20 @@ import { useState } from "react";
 
 function FormulaireInscription(){
   const [nom, setNom] = useState('');
-  const [prenom, setPrenom] = useState('');
+  const [email, setEmail] = useState('');
 
   function handleChangeNom(event) {
     setNom(event.target.value);
     
   }
-  function handleChangePrenom(event) {
+  function handleChangeEmail(event) {
     
-    setPrenom(event.target.value);
+    setEmail(event.target.value);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`Le nom saisi est : ${nom} \n Le prénom saisi est : ${prenom}`);
+    alert(`Le nom saisi est : ${nom} \n L'email saisi est : ${email}`);
   }
 
   return (
@@ -25,8 +25,8 @@ function FormulaireInscription(){
         <input type="text" value={nom} onChange={handleChangeNom} />
       </label>
       <label>
-        Prénom :
-        <input type="text" value={prenom} onChange={handleChangePrenom} />
+        Email :
+        <input type="email" value={email} onChange={handleChangeEmail} />
       </label>
       <button type="submit">Soumettre</button>
     </form>
